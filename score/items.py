48788ddf9d7ee548e417f9f506c6a3b52080bd62
Takuya Tanaka <tanaka@isquare.co.jp>
2017-06-14 09:54:32 +0900

    開始前の扱い TODO: テスト書く TODO: イニング情報

--- score/items.py
+++ score/items.py
@@ -5,12 +5,14 @@ class Game(scrapy.Item):
     team1 = scrapy.Field()
     score0 = scrapy.Field()
     score1 = scrapy.Field()
+    playing = scrapy.Field()
 
 
 
     def to_str(self):
-
-        return "{0} {1} - {2} {3}".format(self["team0"], self["score0"], self["score1"], self["team1"])
-
+        if self["playing"]:
+            return "{0} {1} - {2} {3}".format(self["team0"], self["score0"], self["score1"], self["team1"])
+        else:
+            return "{0} - {1} (開始前)".format(self["team0"], self["team1"])
 
 
--- score/spiders/npb.py
+++ score/spiders/npb.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
+
 import scrapy
 from score.items import Game
 import urllib.request, urllib.parse
+import re
 
 class NpbSpider(scrapy.Spider):
     name = "npb"
     allowed_domains = ["https://baseball.yahoo.co.jp/npb/"]
     start_urls = ['https://baseball.yahoo.co.jp/npb/']
-#    start_urls = ['https://baseball.yahoo.co.jp/npb/schedule/?date=20170613']
+    start_urls = ['https://baseball.yahoo.co.jp/npb/schedule/?date=20170613']
 
     def parse(self, response):
 
@@ -15,7 +17,6 @@ class NpbSpider(scrapy.Spider):
 
         for game in response.css(".NpbScoreBg table.teams"):
             mygame=Game({"team0": "", "team1":"", "score0":0, "score1":0})
-
 
 #            for team in game.css("tr td.yjMS span a::text"):
 #                print(team.extract())
@@ -25,21 +26,23 @@ class NpbSpider(scrapy.Spider):
 
             g=0
 
-            for score in game.css("table tr td.score_r"):
-                s=0
-                if score.css("strong"):
-                    s=score.css("strong::text")[0]
-                else:
-                    s=score.css("::text")[0]
+            if re.search("試合前", game.extract()) != None :
+                mygame["playing"]=False
+            else:
+                mygame["playing"]=True
+                for score in game.css("table tr td.score_r"):
+                    s=0
+                    if score.css("strong"):
+                        s=score.css("strong::text")[0]
+                    else:
+                        s=score.css("::text")[0]
 
-                mygame["score{0}".format(g)]=s.extract()
-                g=g+1
+                    mygame["score{0}".format(g)]=s.extract()
+                    g=g+1
 
-#                e=score.extract().strip()
-#                if e != "" :
-#                    print(e)
-
-            print(mygame.to_str())
+#                    print(mygame.to_str())
+                    pass
+                
             mygames.append(mygame)
             print ("---")
 
