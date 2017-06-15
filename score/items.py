a972bfd176b66a179841fbdac5cdbb579408717f
Takuya Tanaka <tanaka@isquare.co.jp>
2017-06-15 09:00:31 +0900

    試合終了の表示

--- score/items.py
+++ score/items.py
@@ -6,12 +6,16 @@ class Game(scrapy.Item):
     score0 = scrapy.Field()
     score1 = scrapy.Field()
     playing = scrapy.Field()
+    end = scrapy.Field()
 
 
 
     def to_str(self):
+
+        end_str="(終了)" if self["end"] else ""
+
         if self["playing"]:
-            return "{0} {1} - {2} {3}".format(self["team0"], self["score0"], self["score1"], self["team1"])
+            return "{0} {1} - {2} {3} {4}".format(self["team0"], self["score0"], self["score1"], self["team1"], end_str)
         else:
             return "{0} - {1} (開始前)".format(self["team0"], self["team1"])
 
--- score/spiders/npb.py
+++ score/spiders/npb.py
@@ -4,25 +4,43 @@ import scrapy
 from score.items import Game
 import urllib.request, urllib.parse
 import re
+import pickle
+
+import sys
+import syslog
+
+
 
 class NpbSpider(scrapy.Spider):
     name = "npb"
     allowed_domains = ["https://baseball.yahoo.co.jp/npb/"]
     start_urls = ['https://baseball.yahoo.co.jp/npb/']
-    start_urls = ['https://baseball.yahoo.co.jp/npb/schedule/?date=20170613']
+#    start_urls = ['https://baseball.yahoo.co.jp/npb/schedule/?date=20170613']
 
     def parse(self, response):
 
+        syslog.openlog(sys.argv[0], syslog.LOG_PID|syslog.LOG_PERROR, syslog.LOG_LOCAL6)
+        syslog.syslog(syslog.LOG_INFO, 'start')
+        
         mygames=[]
 
+
+        if re.search("本日の試合はあり", response.css(".NpbScoreBg")[0].extract()):
+            syslog.syslog(syslog.LOG_INFO, '試合なし')
+            syslog.closelog()
+            return 
+
         for game in response.css(".NpbScoreBg table.teams"):
-            mygame=Game({"team0": "", "team1":"", "score0":0, "score1":0})
+            mygame=Game({"team0": "", "team1":"", "score0":0, "score1":0, "end" : False})
 
 #            for team in game.css("tr td.yjMS span a::text"):
 #                print(team.extract())
 
             mygame["team0"]=game.css("tr td.yjMS span a::text")[0].extract()
             mygame["team1"]=game.css("tr td.yjMS span a::text")[1].extract()
+            #print(mygame["inning"])
+            # mygame["status"]=game.css("tr td.yjMS span a::text")[1].extract()
+            # "end yjMSt bt bb"
 
             g=0
 
@@ -30,6 +48,10 @@ class NpbSpider(scrapy.Spider):
                 mygame["playing"]=False
             else:
                 mygame["playing"]=True
+                
+                if re.search("結果", game.extract()) != None :
+                    mygame["end"]=True
+                
                 for score in game.css("table tr td.score_r"):
                     s=0
                     if score.css("strong"):
@@ -40,20 +62,52 @@ class NpbSpider(scrapy.Spider):
                     mygame["score{0}".format(g)]=s.extract()
                     g=g+1
 
-#                    print(mygame.to_str())
+                    print(mygame.to_str())
                     pass
                 
             mygames.append(mygame)
             print ("---")
 
         print("   ".join([ g.to_str() for g in mygames]))
-        self.mypost("   ".join([ g.to_str() for g in mygames]))
+
+        if self.is_changed(mygames):
+            syslog.syslog(syslog.LOG_INFO, 'changed')
+            self.mypost("   ".join([ g.to_str() for g in mygames]))
     
+        syslog.closelog()
 
 
+
+    def is_changed(self, games):
+
+        syslog.syslog(syslog.LOG_INFO, 'is_changed??')
+        try:
+            with open('entry.pickle', 'rb') as f:
+                before=pickle.load(f)
+                print(before)
+                f.close()
+                syslog.syslog(syslog.LOG_INFO, 'read.')
+            
+                if games != before :
+                    with open('entry.pickle', 'wb') as f:
+                        pickle.dump(games, f)
+                        syslog.syslog(syslog.LOG_INFO, 'changed. wrote.')
+                        return True
+        except:
+            print(sys.exc_info())
+            with open('entry.pickle', 'wb') as f:
+                pickle.dump(games, f)
+
+            return True
+            
+        return False
+        pass
+
+        
     def mypost(self, result):
+        spacer="　　　　　　　　"
         data = {
-                "str": result,
+            "str": spacer+"【むくむくニュース】"+result,
         }
     # ここでエンコードして文字→バイトにする！
         url="http://192.168.207.42:5000/news"
