e4f901469e2512671a24eaf1202af6d9e69b47e4
Your Name <you@example.com>
2020-07-03 22:05:02 +0900

    Setup prettier

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-const request = require('request')
-const {
-  JSDOM
-} = require('jsdom')
+//  node_modules/.bin/prettier --write index.js --print-width=120
 
-request('https://baseball.yahoo.co.jp/npb/', (e, response, body) => {
-    if (e) {
-      console.error(e)
-    }
+const request = require("request");
+const { JSDOM } = require("jsdom");
 
-    try {
-      const dom = new JSDOM(body)
-        const q = (s)=>dom.window.document.querySelector(s)
-        const qa= (s)=>dom.window.document.querySelectorAll(s)
+request("https://baseball.yahoo.co.jp/npb/", (e, response, body) => {
+  if (e) {
+    console.error(e);
+  }
 
-        const games=qa('.bb-score__item')
-        Array.from(games,  g => {
-        const q = (s)=>g.querySelector(s)
-          const team_l = q('div.bb-score__team > p.bb-score__homeLogo').textContent.trim()
-          const team_r = q('div.bb-score__team > p.bb-score__awayLogo').textContent.trim()
+  try {
+    const dom = new JSDOM(body);
+    const q = (s) => dom.window.document.querySelector(s);
+    const qa = (s) => dom.window.document.querySelectorAll(s);
 
-            const score_l = q('.bb-score__score.bb-score__score--left').textContent.trim()
-            const score_r = q('.bb-score__score.bb-score__score--right').textContent.trim()
+    const gamesInfo = [];
+    const games = qa(".bb-score__item");
+    Array.from(games, (g) => {
+      const q = (s) => g.querySelector(s);
+      const team_l = q("div.bb-score__team > p.bb-score__homeLogo").textContent.trim();
+      const team_r = q("div.bb-score__team > p.bb-score__awayLogo").textContent.trim();
 
-            const inning = q('.bb-score__link').textContent.trim()
-            //        .textContent.trim()
-            console.log(`${team_l} ${score_l}-${score_r} ${team_r} (${inning})`)
-        })
-
-    } catch (e) {
-      console.error(e)
-    }
-})
+      if (q(".bb-score__score.bb-score__score--left")) {
+        const score_l = q(".bb-score__score.bb-score__score--left").textContent.trim();
+        const score_r = q(".bb-score__score.bb-score__score--right").textContent.trim();
+        const inning = q(".bb-score__link").textContent.trim();
+        gamesInfo.push(`${team_l} ${score_l}-${score_r} ${team_r} (${inning})`);
+      } else if (q(".bb-score__link")) {
+        const result = q(".bb-score__link").textContent.trim();
+        gamesInfo.push(`${team_l} - ${team_r} (${result})`);
+      }
+    });
+    console.log(gamesInfo);
+  } catch (e) {
+    console.error(e);
+  }
+});
