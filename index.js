36b6e3bb5ddbef8fedb32435d5edbc85e56b0664
Your Name <you@example.com>
2020-07-04 06:07:38 +0900

    post data

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
+// 2020シーズン版　ヤフーニュースをうちの電光掲示板にPOST
 //  node_modules/.bin/prettier --write index.js --print-width=120
+'use strict'
 
-const request = require("request");
-const { JSDOM } = require("jsdom");
+const request = require('request')
+const { JSDOM } = require('jsdom')
+require('dotenv').config()
 
-request("https://baseball.yahoo.co.jp/npb/", (e, response, body) => {
-  if (e) {
-    console.error(e);
-  }
+const YAHOO_URL = process.env.yahoo_url
 
-  try {
-    const dom = new JSDOM(body);
-    const q = (s) => dom.window.document.querySelector(s);
-    const qa = (s) => dom.window.document.querySelectorAll(s);
+request(YAHOO_URL, (e, response, body) => {
+    if (e) {
+        console.error(e)
+    }
 
-    const gamesInfo = [];
-    const games = qa(".bb-score__item");
-    Array.from(games, (g) => {
-      const q = (s) => g.querySelector(s);
-      const team_l = q("div.bb-score__team > p.bb-score__homeLogo").textContent.trim();
-      const team_r = q("div.bb-score__team > p.bb-score__awayLogo").textContent.trim();
+    try {
+        const dom = new JSDOM(body)
+        const q = (s) => dom.window.document.querySelector(s)
+        const qa = (s) => dom.window.document.querySelectorAll(s)
 
-      if (q(".bb-score__score.bb-score__score--left")) {
-        const score_l = q(".bb-score__score.bb-score__score--left").textContent.trim();
-        const score_r = q(".bb-score__score.bb-score__score--right").textContent.trim();
-        const inning = q(".bb-score__link").textContent.trim();
-        gamesInfo.push(`${team_l} ${score_l}-${score_r} ${team_r} (${inning})`);
-      } else if (q(".bb-score__link")) {
-        const result = q(".bb-score__link").textContent.trim();
-        gamesInfo.push(`${team_l} - ${team_r} (${result})`);
-      }
-    });
-    console.log(gamesInfo);
-  } catch (e) {
-    console.error(e);
-  }
-});
+        const gamesInfo = []
+        const games = qa('.bb-score__item')
+
+        Array.from(games, (g) => {
+            const q = (s) => g.querySelector(s)
+            const team_l = q('div.bb-score__team > p.bb-score__homeLogo').textContent.trim()
+            const team_r = q('div.bb-score__team > p.bb-score__awayLogo').textContent.trim()
+
+            if (q('.bb-score__score.bb-score__score--left')) {
+                const score_l = q('.bb-score__score.bb-score__score--left').textContent.trim()
+                const score_r = q('.bb-score__score.bb-score__score--right').textContent.trim()
+                const inning = q('.bb-score__link').textContent.trim()
+                gamesInfo.push(`${team_l} ${score_l}-${score_r} ${team_r} (${inning})`)
+            } else if (q('.bb-score__link')) {
+                const result = q('.bb-score__link').textContent.trim()
+                gamesInfo.push(`${team_l}-${team_r} (${result})`)
+            }
+        })
+        console.log(gamesInfo)
+        ledPost(gamesInfo.join('　'))
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+const ledPost = (s) => {
+    const spacer = '　　　　　　　　'
+    const data = {
+        str: spacer + '【むくむくニュース】' + s,
+    }
+
+    const URL = process.env.led_url
+
+    const request = require('request')
+    const options = {
+        uri: URL,
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+        },
+        form: data,
+    }
+    request.post(options, function (error, response, body) {})
+}
