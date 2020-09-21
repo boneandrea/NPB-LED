bf26e96fcfd973de6be7f382dd962ec19a8f8520
Your Name <you@example.com>
2020-09-21 21:53:52 +0900

    Setup prettier(npm run lint)

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 
 const request = require('request')
 const { JSDOM } = require('jsdom')
-const parseGame=require("./parseGame");
+const parseGame = require('./parseGame')
 require('dotenv').config()
 
 const YAHOO_URL = process.env.yahoo_url
-const GAMES_FILE="./games.json"
+const GAMES_FILE = './games.json'
 
 request(YAHOO_URL, (e, response, body) => {
     if (e) {
@@ -20,52 +20,50 @@ request(YAHOO_URL, (e, response, body) => {
         const q = (s) => dom.window.document.querySelector(s)
         const qa = (s) => dom.window.document.querySelectorAll(s)
 
-        const date=q(".bb-head02__title").textContent
+        const date = q('.bb-head02__title').textContent
         const gamesInfo = []
         const games = qa('.bb-score__item')
 
-        const params={
-            notOnPlayGames:0
-        };
+        const params = {
+            notOnPlayGames: 0,
+        }
 
-        Array.from(games, (g) => {
-            const game=parseGame.parse(g,params);
-            gamesInfo.push(game);
+        Array.from(games, async (g) => {
+            const game = await parseGame.parse(g, params)
+            gamesInfo.push(game)
         })
 
-        const data={
+        const data = {
             date,
             games: gamesInfo,
         }
 
-        const lastData=getLastData()
-        const fs=require("fs")
+        const lastData = getLastData()
+        const fs = require('fs')
         fs.writeFileSync(GAMES_FILE, JSON.stringify(data))
 
-        const json=JSON.stringify(data)
+        const json = JSON.stringify(data)
 
-        if(json !== lastData){
-            if(games.length !== params.notOnPlayGames){
+        if (json !== lastData) {
+            if (games.length !== params.notOnPlayGames) {
                 ledPost(data.games.join('　'))
             }
         }
-
     } catch (e) {
         console.error(e)
-        postErrorToSlack(`[NPB] send failed;${e.message}`);
+        postErrorToSlack(`[NPB] send failed;${e.message}`)
     }
 })
 
-const getLastData=()=>{
-    const fs=require("fs")
-    try{
+const getLastData = () => {
+    const fs = require('fs')
+    try {
         return fs.readFileSync(GAMES_FILE, 'utf-8')
     } catch (e) {
         console.error(e)
-        return ""
+        return ''
     }
 }
-
 
 const ledPost = (s) => {
     const spacer = '　　　　　　　　'
@@ -84,24 +82,24 @@ const ledPost = (s) => {
         form: data,
     }
     request.post(options, function (error, response, body) {
-        body=JSON.parse(body)
-        if(!body.result){
-            console.log(error);
-            console.log(response);
+        body = JSON.parse(body)
+        if (!body.result) {
+            console.log(error)
+            console.log(response)
         }
     })
 }
 
-const postErrorToSlack=(s)=>{
+const postErrorToSlack = (s) => {
     const request = require('request')
     const options = {
         uri: process.env.slack_hook,
         headers: {
             'Content-type': 'application/json',
         },
-        json:{
-            text: s
-        }
+        json: {
+            text: s,
+        },
     }
     request.post(options, function (error, response, body) {})
-};
+}
--- parseGame.js
+++ parseGame.js
@@ -1,33 +1,81 @@
-module.exports={
-    parse(g,params){
+const request = require('request')
+const { JSDOM } = require('jsdom')
 
+module.exports = {
+    parse: async function (g, params) {
         const q = (s) => g.querySelector(s)
         const team_l = q('div.bb-score__team > p.bb-score__homeLogo').textContent.trim()
         const team_r = q('div.bb-score__team > p.bb-score__awayLogo').textContent.trim()
 
         if (q('.bb-score__score.bb-score__score--left')) {
-            if(q('.bb-score__link').textContent.trim() === "試合終了"){
-                const win_pitcher=q(".bb-score__player.bb-score__player--win").textContent;
-                const lose_pitcher=q(".bb-score__player.bb-score__player--lose").textContent;
+            if (q('.bb-score__link').textContent.trim() === '試合終了') {
+                const win_pitcher = q('.bb-score__player.bb-score__player--win').textContent
+                const lose_pitcher = q('.bb-score__player.bb-score__player--lose').textContent
 
                 const score_l = q('.bb-score__score.bb-score__score--left').textContent.trim()
                 const score_r = q('.bb-score__score.bb-score__score--right').textContent.trim()
                 const inning = q('.bb-score__link').textContent.trim()
-                return `${team_l} 対 ${team_r} : ${score_l}-${score_r} (${inning}) [勝:${win_pitcher} 負:${lose_pitcher}]`;
-            }else{
+                return `${team_l} 対 ${team_r} : ${score_l}-${score_r} (${inning}) [勝:${win_pitcher} 負:${lose_pitcher}]`
+            } else {
                 // 試合中
                 const score_l = q('.bb-score__score.bb-score__score--left').textContent.trim()
                 const score_r = q('.bb-score__score.bb-score__score--right').textContent.trim()
                 const inning = q('.bb-score__link').textContent.trim()
-                return `${team_l} 対 ${team_r} : ${score_l}-${score_r} (${inning})`;
-            }
 
+                const gameLiveUrl = q('.bb-score__content').getAttribute('href')
+                const self = this
+                const p_info = await self.getLiveInfo(gameLiveUrl)
+
+                return `${team_l} 対 ${team_r} : [${p_info.home}]${score_l}-${score_r}[${p_info.away}] (${inning})`
+            }
         } else if (q('.bb-score__link')) {
             // 中止とか
-            console.log("2;"+q('.bb-score__link').textContent.trim());
-            const result = q('.bb-score__link').textContent.trim().replace(/見どころ/,"開始前")
-            params.notOnPlayGames++;
-            return `${team_l}-${team_r} (${result})`;
+            console.log('2;' + q('.bb-score__link').textContent.trim())
+            const result = q('.bb-score__link').textContent.trim().replace(/見どころ/, '開始前')
+            params.notOnPlayGames++
+            return `${team_l}-${team_r} (${result})`
         }
-    }
-};
+    },
+
+    getLiveInfo: async function (url) {
+        return new Promise((resolve, reject) => {
+            console.log(url)
+            let p_h = ''
+            let p_a = ''
+            request(url, (e, response, body) => {
+                if (e) {
+                    console.error(e)
+                }
+
+                try {
+                    const dom = new JSDOM(body)
+                    const q = (s) => dom.window.document.querySelector(s)
+                    const qa = (s) => dom.window.document.querySelectorAll(s)
+
+                    const battery_home = q(
+                        '#gm_memh > table:nth-child(4) > tbody:nth-child(1) > tr.bb-splitsTable__row > td'
+                    )
+                        .textContent.replace(/\r?\n/g, '')
+                        .replace(/ /g, '')
+                        .replace(/、/g, ',')
+                    const battery_away = q(
+                        '#gm_mema > table:nth-child(4) > tbody:nth-child(1) > tr.bb-splitsTable__row > td'
+                    )
+                        .textContent.replace(/\r?\n/g, '')
+                        .replace(/ /g, '')
+                        .replace(/、/g, ',')
+
+                    if (battery_home.match(/,([^,]*)\-/)) {
+                        p_h = RegExp.$1
+                    }
+                    if (battery_away.match(/,([^,]*)\-/)) {
+                        p_a = RegExp.$1
+                    }
+                } catch (e) {
+                    reject(e)
+                }
+                resolve({ home: p_h, away: p_a })
+            })
+        })
+    },
+}
